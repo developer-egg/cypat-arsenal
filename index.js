829bbd09541572948ac37d092311fe214b6db25b
Egg <88744505+developer-egg@users.noreply.github.com>
2022-12-09 17:21:00 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ for (let tab of Array.from(tabs)) {
         switch(tab_index) {
             case 0:
                 document.getElementById("content-link").textContent = "Maytham's Ubuntu Checklist"
-                document.getElementById("content-link-2").setAttribute("href", "https://matees.net/blog/linux-checklist/")
+                document.getElementById("content-link").setAttribute("href", "https://matees.net/blog/linux-checklist/")
                 break;
             case 1:
                 document.getElementById("content-link").textContent = "Maytham's Ubuntu Script"
-                document.getElementById("content-link-2").setAttribute("href", "https://github.com/maytees/cypat-ubuntu-script")
+                document.getElementById("content-link").setAttribute("href", "https://github.com/maytees/cypat-ubuntu-script")
                 break;
             case 2:
                 document.getElementById("content-link").textContent = "There's nothing here yet :C"
-                document.getElementById("content-link-2").setAttribute("href", "")
+                document.getElementById("content-link").setAttribute("href", "")
                 break;
             case 3:
                 document.getElementById("content-link").textContent = "There's nothing here yet :C"
-                document.getElementById("content-link-2").setAttribute("href", "")
+                document.getElementById("content-link").setAttribute("href", "")
                 break;
             default:
                 document.getElementById("content-link").textContent = "Invalid Tab??"
